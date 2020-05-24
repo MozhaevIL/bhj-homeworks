2fbdec05b5f205588b30d61c2648f8c4e12d1b1e
MozhaevIL <ptitzzz@gmail.com>
2020-05-24 19:38:41 +0300

    slider fix

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,35 +1,39 @@
 const sliderItems = Array.from(document.getElementsByClassName("slider__item"));
-let slideNumber = 0;
+let activeItemIndex = 0;
+
+function showActiveItem(index) {
+    const lastActiveItem = document.querySelector(".slider__item_active");
+    lastActiveItem.classList.remove("slider__item_active");
+    sliderItems[index].classList.add("slider__item_active");
+}
 
 const prevButton = document.querySelector(".slider__arrow_prev");
 prevButton.onclick = () => {
-    const activeSlide = document.querySelector(".slider__item_active")
-    activeSlide.className = "slider__item";
-    if (slideNumber === 0) {
-        slideNumber = sliderItems.length - 1;
+    if (activeItemIndex === 0) {
+        activeItemIndex = sliderItems.length - 1;
     } else {
-        slideNumber -= 1;
+        activeItemIndex -= 1;
     }
-    sliderItems[slideNumber].className = "slider__item slider__item_active";
+    showActiveItem(activeItemIndex);
 }
 
 const nextButton = document.querySelector(".slider__arrow_next");
 nextButton.onclick = () => {
-    const activeSlide = document.querySelector(".slider__item_active")
-    activeSlide.className = "slider__item";
-    if (slideNumber === sliderItems.length - 1) {
-        slideNumber = 0;
+    if (activeItemIndex === sliderItems.length - 1) {
+        activeItemIndex = 0;
     } else {
-        slideNumber += 1;
+        activeItemIndex += 1;
     }
-    sliderItems[slideNumber].className = "slider__item slider__item_active";
+    showActiveItem(activeItemIndex);
 }
+
 
 const dots = Array.from(document.getElementsByClassName("slider__dot"));
 for (let dot of dots) {
     dot.onclick = () => {
-        const activeSlide = document.querySelector(".slider__item_active")
-        activeSlide.className = "slider__item";
-        sliderItems[dots.indexOf(dot)].className = "slider__item slider__item_active";
+        dots[activeItemIndex].classList.remove("slider__dot_active");
+        dot.classList.add("slider__dot_active");
+        activeItemIndex = dots.indexOf(dot);
+        showActiveItem(activeItemIndex);
     }
 }
