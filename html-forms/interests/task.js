7d31f7184deb48144fe821afb03a5a57490808a6
MozhaevIL <ptitzzz@gmail.com>
2020-05-26 00:14:32 +0300

    interests basic solutions added

---
+++ html-forms/interests/task.js
@@ -0,0 +1,26 @@
+const interests = Array.from(document.querySelectorAll("li.interest"));
+
+for (interest of interests)  {
+    const interestCheckbox = interest.querySelector(".interest__check");
+    const subInterests = Array.from(interest.querySelectorAll("li.interest"));
+    const subInterestsCheckboxes = Array.from(interest.querySelectorAll("ul.interests .interest__check"));
+    
+    
+    if(subInterests.length > 0) {
+        
+        interestCheckbox.addEventListener("change", () => {
+            console.log(subInterestsCheckboxes);
+            if(interestCheckbox.checked) {
+                for (subInterestsCheckbox of subInterestsCheckboxes) {
+                 subInterestsCheckbox.checked = true;
+                }
+            }  else {
+                for (subInterestsCheckbox of subInterestsCheckboxes) {
+                  subInterestsCheckbox.checked = false;
+                }
+            }
+        }
+        
+        )
+    }
+}
